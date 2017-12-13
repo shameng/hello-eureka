e45e163fb6194fae0f7b671dc5fead32be358987
imagedt_xindemeng <dingbang@imagedt.com>
2017-12-13 20:51:44 +0800

    第一次提交

---
+++ src/main/java/com/meng/helloeureka/HelloEurekaApplication.java
@@ -0,0 +1,14 @@
+package com.meng.helloeureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class HelloEurekaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloEurekaApplication.class, args);
+	}
+}

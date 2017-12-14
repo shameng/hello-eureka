9a60a68cec18ee11f70723f5494c8819a2d61610
imagedt_xindemeng <dingbang@imagedt.com>
2017-12-14 20:50:26 +0800

    ...

--- src/main/java/com/meng/helloeureka/HelloEurekaApplication.java
+++ src/main/java/com/meng/helloeureka/HelloEurekaApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class HelloEurekaApplication {
